03efc8fa0c41e126418ea07c973be1b1146e024c
Daniel Cornelio <jdcornelio89@gmail.com>
2025-03-13 22:24:02 -0600

    Se agregan validaciones

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import './App.css'
 import Navbar from './components/Navbar/Navbar'
 import Home from './components/Home/Home'
 import Footer from './components/Footer/Footer'
+import Register from './components/Register/Register'
+import Login from './components/Login/Login'
 
 function App() {
 
   return (
     <>
       <Navbar/>
-      <Home/>
+      {/* <Home/> */}
+      <Register/>
+      {/* <Login/> */}
       <Footer/>
     </>
   )
